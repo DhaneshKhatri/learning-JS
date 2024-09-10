b57524eb53960645f3567ce6ccbb9b274e8d59cd
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-10 09:23:33 +0000

    varaibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "kumardhanesh951@gmail.com"
+var accountPassword ="12345"
+accountCity ="TandoAdam"
+
+// accountId =2 //not allowed
+accountEmail="kumardha@gmail.com"
+accountPassword ="2121212"
+accountCity ="Benagaluru"
+let accountState;
+
+console.table([accountId,accountEmail, accountPassword,accountCity,accountState] )
