fa1696eb4f64f61eefb521e05bea0929c567c570
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-15 08:20:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ;//it will treat our all code as newer version of Javascript
+//alert (3+3) we are using node js not browser 
+
+
+let name = "Dhanesh" 
+let age = 25
+let isLoggedIn =false
+
+//PREMATIVE DATA TYPES
+
+//types of Data in javascript 
+//num => 2 to power 53 max value
+//BigInt
+//string => ""
+//boolean =>true/false  possible only two values yes/no or true/flase
+// null => standAlone value //(is also an Object)
+// undefined => 
+// Symbol => unique
+
+
+
+//object
+console.log(typeof "Dhanesh")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
