2a0dd0ed72c894e53274f058f3ed5713577c99a4
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-15 17:47:57 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score = undefined
-console.log(typeof score)
+// console.log(typeof score)
 let valueinNum = Number(score);
-console.log(typeof valueinNum)
-console.log(valueinNum)
+// console.log(typeof valueinNum)
+// console.log(valueinNum)
 
 
 // "33" => 33
@@ -11,3 +11,20 @@ console.log(valueinNum)
 
 
 //let we have a boolean value 
+let isloggedIn ="hitesh"
+let booleanisLoggedin = Boolean(isloggedIn)
+// console.log(booleanisLoggedin)
+
+//1 => true, 0=> false.
+// "" => false 
+//"hitesh" => true
+
+
+//In same way we also covert number in to string
+ let aNum = 34
+ let convertNumtostring =String(aNum)
+ console.log(convertNumtostring)
+ console.log(typeof convertNumtostring);
+
+
+ 
