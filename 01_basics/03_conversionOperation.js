f039747fadf51c7ca217ba3df886dce6ee138d40
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-15 08:56:14 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = undefined
+console.log(typeof score)
+let valueinNum = Number(score);
+console.log(typeof valueinNum)
+console.log(valueinNum)
+
+
+// "33" => 33
+//"33abc" =>NaN
+//true => 1 ,false =>0
+
+
+//let we have a boolean value 
