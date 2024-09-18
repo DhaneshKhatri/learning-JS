a7a11360a4dcb5f6da637bd603c2c689d70c4ecc
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-18 08:11:29 +0000

    conversion operator last

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,8 +23,33 @@ let booleanisLoggedin = Boolean(isloggedIn)
 //In same way we also covert number in to string
  let aNum = 34
  let convertNumtostring =String(aNum)
- console.log(convertNumtostring)
- console.log(typeof convertNumtostring);
+//  console.log(convertNumtostring)
+//  console.log(typeof convertNumtostring);
 
 
- 
+ //***************OPERATIONS********************
+
+ let value =3
+ let negvalue = -value
+//  console.log(negvalue)
+//  console.log(2+2)
+//  console.log(2-2)
+//  console.log(2*2)
+//  console.log(2**3)
+//  console.log(2/3)
+//  console.log(2%3)
+
+
+// console.log("1" + 2)  //output => 12
+// console.log(1 +"2")    //output => 12
+// console.log("1" + 2 +2)   //output => 122
+// console.log(1 + 2 +"2")   //output => 32 
+ //NOTE : Jab start me string ho to agay wale sare elements ko as a string treat karega.
+
+
+// console.log(true);  //output => true
+// console.log(+true);  //output => 1
+// console.log(+""); //output => 0
+//not recommended this type of code
+
+
