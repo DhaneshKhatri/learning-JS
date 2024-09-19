34d6a90c53b75070b55b0b917bc30da7c8d7a99b
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-19 06:21:08 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+//it's easy to compare same data types
+
+//Different data type comparisons
+// console.log(null > 0);  //false
+// console.log(null < 0); //false
+// console.log( null == 0); //false
+// console.log(null <= 0);  //true
+// console.log(null >= 0); //true
+
+console.log(undefined == 0 ); //false
+console.log(undefined > 0 );//false
+console.log(undefined <0 );//false
+console.log(undefined >=0 );//false
+console.log(undefined <=0 );//fasle
+
+
+
+// Strict check when we use === then this will comapre strict ..like also check data types
+
+console.log( "2" === 2) // we get false because string and number are not equal
+console.log("2" == 2) // we get true as output, because it will convert the string into number
+
+
