aeaf904ce023bbe40b44ed3bd435879db890e968
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-09-22 18:20:19 +0000

    number & maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400;
+// const balance=  new Number(400)
+// console.log(score)
+// console.log(balance)
+
+const newN =score.toString()
+// console.log(newN.length)
+// console.log(score.toFixed(2))
+
+const OtherNum =123.87595
+// console.log(OtherNum.toPrecision(2))
+
+const hunred = 100000
+// console.log(hunred.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+//  console.log(Math)
+//  console.log(Math.abs(-4))     //changes negative value into positive
+//  console.log(Math.round(4.5))   // rounds of number and give us an integer
+//  console.log(Math.ceil(3.2))    // agr 3 se 0.001 bi ziyada huw to yeh usko 4 me convert kerdega
+//  console.log(Math.floor(3.2))   
+//  console.log(Math.min(3,4,1,3,5))  // we get minimum value of array
+//  console.log(Math.max(3,4,1,3,5)) // we get highest value in array
+
+//  console.log(Math.random()); //we get 0.69987878987687 random value b/w 0 and 1
+//  console.log((Math.random()*10)+1 )
+const min =1
+const max =6
+console.log(Math.floor(Math.random()*(max - min +1) +min));
+
