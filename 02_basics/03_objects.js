8eac1dc3a5f2cfd19d69c5dcedaf2d20f839ec95
DhaneshKhatri <khatridhanesh951@gmail.com>
2024-10-09 13:03:28 +0000

    objects lec1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,17 +2,29 @@
 // (1) litteral (se singleton ni banta)   // (2) constructor(se always singleton banta he) => object.create
 
 //singleton (mtlab ye apni tarah ka ek hi object he)
+const mySym =Symbol("key1")
 
+const Jsuser={
+    name :"Dhanesh",
+    "fullname" :"vishal kumar",
+    [mySym] : "mykey1",
+    age :18,
+    location :"jaipur",
+    email:"kumardhanesh950@gmail.com",
+    isloggedIn : false,
 
-//object literals
-const jsuser ={
-    "name" : "Dhanesh kumar",         // by defualt javascript name as a string mean in real "name" he
-    age :19,
-    email: "kumardhanesh950@gmail.com",
-    isloggedIn :false,
-    lastloggIndays:["monday","saturday"]
 }
-// Now let's access the values of object
-console.log(jsuser["email"]) // we get => Dhanesh kumar
-// but we have another method also agr hume value object me ese mile "name" :"Dhanesh kumar" to isko hume sirf ese hi access ker sakte
-// console.log(jsuser["name"]) //we get
+// console.log(Jsuser.email)
+// we can change the values of key by
+Jsuser.email ="dhaneshmth46@gmail.com"
+// console.log(Jsuser);    Note agr hum chahte hen to value change ni ker paye koi to hum object.freeze use kerenge
+// Object.freeze(Jsuser)
+Jsuser.name ="ashok" // dekhen output me name Dhanesh hi raha
+// console.log(Jsuser)
+
+
+Jsuser.greeting =function(){
+console.log(`hello JSuser,${this.name}`);
+
+}
+console.log(Jsuser.greeting())
